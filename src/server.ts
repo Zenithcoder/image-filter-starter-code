14ef2c350fe86d418de6bfa85d389fe9c669859c
mac <awonusiolajide@yahoo.com>
2021-11-25 15:10:54 +0100

    added typescript

--- src/server.ts
+++ src/server.ts
@@ -10,28 +10,27 @@ const path = require("path");
   const app = express();
 
   // Set the network port
-  const port = process.env.PORT || 8082;
+  const port: any = process.env.PORT || 8082;
 
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  app.get("/filteredimage", async (req, res) => {
+  /*app.get("/filteredimage", async (req:any, res:any) => {
 
-    const keys = Object.keys(req.query);
+    const keys:string[] = Object.keys(req.query);
     // Do we have enough query parameters?
     if (!keys.length){
       return res.status(422).send('Please select an image url to upload');
     } 
   
-
-    const testFolder = __dirname +'/util/tmp/';
+    const testFolder:string = __dirname +'/util/tmp/';
    
     let files: any[] = [];
     
     const getFilesRecursively = (directory: any) => {
-      const filesInDirectory = fs.readdirSync(directory);
+      const filesInDirectory:string[] = fs.readdirSync(directory);
       for (const file of filesInDirectory) {
-        const absolute = path.join(directory, file);
+        const absolute:string = path.join(directory, file);
         if (fs.statSync(absolute).isDirectory()) {
             getFilesRecursively(absolute);
         } else {
@@ -44,16 +43,31 @@ const path = require("path");
     deleteLocalFiles(files);
 
 
-    let image_url = req.query.image_url;
-    let filteredpath = filterImageFromURL(image_url);
+    let image_url : string = req.query.image_url;
+    let filteredpath : any= filterImageFromURL(image_url);
     
     res.status(422).sendFile(await filteredpath);
 
   });
+*/
 
+  app.get("/filteredimage/", async (req:any, res:any) => {
+    try {
+      let image_url : string = req.query.image_url;
+      if (!image_url) {
+        return res.status(400).send("Please select an image url to upload");
+      }
+ 
+      const path: string = await filterImageFromURL(image_url);
+      res.sendFile(path);
+      res.on('finish', () => deleteLocalFiles([path]));
+    } catch {
+      return res.status(500).send({ error: 'Unable to process your request' });
+    }
+  });
   // Root Endpoint
   // Displays a simple message to the user
-  app.get("/", async (req, res) => {
+  app.get("/", async (req: any, res: any) => {
     res.send("Welcome to Image filter service")
   });
 
